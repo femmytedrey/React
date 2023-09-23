b157bcc558fd4638cbd881c2d25b5111f23a384a
femmytedrey <femmytedrey@gmail.com>
2023-09-23 18:41:44 +0100

    working

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import Button from "./components/button"
 function App() {
   return (
     <div>
-      <Button color = "info" onClick={() => console.log("Femi_Dev")}>My Button</Button>
-      <Button color = "primary" onClick={() => console.log("Femi_Dev")}>My Button</Button>
+      <Button color = "danger" onClick={() => console.log("Femi_Dev")}>My Button</Button>
     </div>
   );
 }
